86a96ece7242449962a2771eec799b364d2f748b
eypino <107155607+eypino@users.noreply.github.com>
2023-12-14 11:25:48 -0300

    revisado registro cliente, extrañamente no pude colocar el color del boton

--- paginas/registroCliente.php
+++ paginas/registroCliente.php
@@ -1,66 +1,129 @@
 <?php
-session_start(); 
-$usuario=$_SESSION['usuario'];
+session_start();
+$usuario = $_SESSION['usuario'];
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro de Clientes - Empleado</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            padding: 0;
+            background-color: #f8f9fa;
+        }
+
+        .navbar {
+            background-color: #343a40;
+            padding: 15px 0;
+            border-bottom: 2px solid #fff;
+        }
+
+        .navbar-brand {
+            color: #fff !important;
+            font-weight: bold;
+        }
+
+        .navbar-nav .nav-link {
+            color: #fff !important;
+            margin-right: 10px;
+            padding: 15px 0;
+        }
+
+        .navbar-toggler-icon {
+            background-color: #fff;
+        }
+
+        .btn-orange {
+            background-color: #fd7e14;
+            color: #fff;
+            border: none;
+            margin-bottom: 3%;
+            margin-top: 1%;
+        }
+
+        .btn-orange:hover {
+            background-color: #343a40;
+            color: #fff;
+        }
+    </style>
+    <style>
+        .login-message {
+            text-align: center;
+        }
+
+        .login-link {
+            display: block;
+            margin-top: 10px;
+        }
+    </style>
 </head>
+
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-            <a class="navbar-brand" href="#">Taller SERVIEXPRESS</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="menuPrincipalUsuario.php">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="registroCliente.php">Registrar Cliente</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="ingresoFactura.php">Administración de Boletas/Facturas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="registrarOrden.php">Registrar Orden de Pedido</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="registroServicio.php">Registrar Servicios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="registroProveedores.php">Registrar Proveedores</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="reservaCitaUsuario.php">Administrar Reservas</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="generarInforme.php">Generar Informes</a>
-                    </li>
-                    <li class="nav-item">
-                    <?php
-                        if($_SESSION!==null){
-                            echo<<<eot
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Registrar Orden- Taller Mecánico SERVIEXPRESS</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <nav class="navbar navbar-expand-lg navbar-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Taller SERVIEXPRESS</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="menuPrincipalUsuario.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="registroCliente.php">Registrar Cliente</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="ingresoFactura.php">Administración de Boletas/Facturas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="registrarOrden.php">Registrar Orden de Pedido</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="registroServicio.php">Registrar Servicios</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="registroProveedores.php">Registrar Proveedores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="reservaCitaUsuario.php">Administrar Reservas</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="generarInforme.php">Generar Informes</a>
+                        </li>
+                        <li class="nav-item">
+                            <?php
+                            if ($_SESSION !== null) {
+                                echo <<<eot
                             <a class="nav-link" href="../includes/db/cerrarSesion.php">Cerrar Sesion</a>
                             eot;
-                        }
-                    ?>
-                    </li>
-                </ul>
+                            }
+                            ?>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
-    </header>
-<main>
-<?php
-            if(isset($usuario)){
-        echo<<<eot
-    <div class="container mt-4">
+        <main>
+            <?php
+            if (isset($usuario)) {
+                echo <<<eot
+        <div class="container mt-4">
         <h1>Registro de Clientes</h1>
         <p>Ingrese los datos del nuevo cliente:</p>
         <form method="POST" name="login"action="../includes/db/registrarCliente.php">
@@ -136,20 +199,23 @@ $usuario=$_SESSION['usuario'];
                     <input type="text" id="region" name="region" class="form-control" required>
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary">Registrar Cliente</button>
+            <button type="submit" class="btn btn-dark">Registrar Cliente</button>
         </form>
     </div>
     </main>
-    eot;}
-    else{
-        echo <<<EOT
+    eot;
+            } else {
+                echo <<<EOT
         <div class="login-message">
         <p>No has iniciado sesión</p>
         <a class="login-link" href="loginUsuario.php">Haz clic aquí para iniciar sesión</a>
     </div>
 EOT;
-        }
-        ?>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.
+            }
+            ?>
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
