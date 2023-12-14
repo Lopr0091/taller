ab82d6a7b6e452d8e4bf0d0c6c683199fa2fe109
eypino <107155607+eypino@users.noreply.github.com>
2023-12-14 18:55:32 -0300

    cambios visuales parte 10000

--- paginas/registroServicio.php
+++ paginas/registroServicio.php
@@ -11,16 +11,76 @@ require '../config.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            padding: auto;
+            background-color: #f8f9fa;
+            margin: auto;
+        }
+
+        .navbar {
+            background-color: #343a40;
+            padding: 15px 0;
+            border-bottom: 2px solid #fff;
+        }
+
+        .navbar-brand {
+            color: #fff !important;
+            font-weight: bold;
+        }
+
+        .navbar-nav .nav-link {
+            color: #fff !important;
+            margin-right: 10px;
+            padding: 15px 0;
+        }
+
+        .navbar-toggler-icon {
+            background-color: #fff;
+        }
+
+        .btn-orange {
+            background-color: #fd7e14;
+            color: #fff;
+            border: none;
+            margin-bottom: 3%;
+            margin-top: 1%;
+        }
+
+        .btn-orange:hover {
+            background-color: #343a40;
+            color: #fff;
+        }
+
+        main {
+            width: 90%;
+            justify-content: center;
+            display: grid;
+
+        }
+    </style>
+    <style>
+        .login-message {
+            text-align: center;
+        }
+
+        .login-link {
+            display: block;
+            margin-top: 10px;
+        }
+    </style>
 </head>
 
 <body>
-<header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
             <a class="navbar-brand" href="#">Taller SERVIEXPRESS</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
                         <a class="nav-link" href="menuPrincipalUsuario.php">Inicio</a>
@@ -47,65 +107,68 @@ require '../config.php';
                         <a class="nav-link" href="generarInforme.php">Generar Informes</a>
                     </li>
                     <li class="nav-item">
-                    <?php
-                        if($_SESSION!==null){
-                            echo<<<eot
+                        <?php
+                        if ($_SESSION !== null) {
+                            echo <<<eot
                             <a class="nav-link" href="../includes/db/cerrarSesion.php">Cerrar Sesion</a>
                             eot;
                         }
-                    ?>
+                        ?>
                     </li>
                 </ul>
             </div>
-        </nav>
-    </header>
+        </div>
+    </nav>
     <main>
         <?php
         if (isset($usuario)) {
             $query = "SELECT id_servicio, nombre_servicio, precio_servicio FROM servicio";
             $result = mysqli_query($conectar, $query);
-        ?>
-            <form method="POST" name="login"action="../includes/db/registrarCliente.php">
-                <div class="form-group">
-                    <label for="servicio">Servicio:</label>
-                    <select class="form-control" id="servicio" name="servicio" onchange="mostrarPrecio()">
-                        <?php
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            echo "<option value='{$row['id_servicio']}' data-precio='{$row['precio_servicio']}'>{$row['nombre_servicio']}</option>";
+            ?>
+            <div class="card mt-4">
+                <div class="card-body">
+                    <h1>Registro de Servicio</h1>
+                    <form method="POST" name="login" action="../includes/db/registrarCliente.php">
+                        <div class="form-group">
+                            <label for="servicio">Servicio:</label>
+                            <select class="form-control" id="servicio" name="servicio" onchange="mostrarPrecio()">
+                                <?php
+                                while ($row = mysqli_fetch_assoc($result)) {
+                                    echo "<option value='{$row['id_servicio']}' data-precio='{$row['precio_servicio']}'>{$row['nombre_servicio']}</option>";
+                                }
+                                ?>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="valor">Valor:</label>
+                            <input type="text" class="form-control" id="valor" name="valor" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="patente">Patente Vehículo:</label>
+                            <input type="text" class="form-control" id="patente" name="patente">
+                        </div>
+                        <div class="form-group">
+                            <label for="run">RUN Cliente:</label>
+                            <input type="text" class="form-control" id="run" name="run">
+                        </div>
+                        <button type="submit" class="btn btn-orange">Enviar</button>
+                    </form>
+
+                    <script>
+                        function mostrarPrecio() {
+                            var select = document.getElementById("servicio");
+                            var valorInput = document.getElementById("valor");
+
+                            // Obtener el precio del servicio seleccionado
+                            var precioSeleccionado = select.options[select.selectedIndex].getAttribute("data-precio");
+
+                            // Mostrar el precio en el campo de valor
+                            valorInput.value = precioSeleccionado;
                         }
-                        ?>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="valor">Valor:</label>
-                    <input type="text" class="form-control" id="valor" name="valor" readonly>
-                </div>
-                <div class="form-group">
-                    <label for="patente">Patente Vehículo:</label>
-                    <input type="text" class="form-control" id="patente" name="patente">
-                </div>
-                <div class="form-group">
-                    <label for="run">RUN Cliente:</label>
-                    <input type="text" class="form-control" id="run" name="run">
-                </div>
-                <button type="submit" class="btn btn-primary">Enviar</button>
-            </form>
-
-            <script>
-                function mostrarPrecio() {
-                    var select = document.getElementById("servicio");
-                    var valorInput = document.getElementById("valor");
-
-                    // Obtener el precio del servicio seleccionado
-                    var precioSeleccionado = select.options[select.selectedIndex].getAttribute("data-precio");
-
-                    // Mostrar el precio en el campo de valor
-                    valorInput.value = precioSeleccionado;
-                }
-            </script>
-        <?php
-            // Liberar el resultado
-            mysqli_free_result($result);
+                    </script>
+                    <?php
+                    // Liberar el resultado
+                    mysqli_free_result($result);
         } else {
             echo <<<EOT
             <div class="login-message">
